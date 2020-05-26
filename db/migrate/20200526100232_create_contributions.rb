1fd6838590fb21dc81b20ccd437c41c8a50e91b1
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-05-26 19:12:02 +0900

    create necessary tables

---
+++ db/migrate/20200526100232_create_contributions.rb
@@ -0,0 +1,13 @@
+class CreateContributions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contributions do |t|
+      t.string :body, null: false
+      t.string :image
+      t.string :status, null: false
+      t.string :priority, null: false
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
