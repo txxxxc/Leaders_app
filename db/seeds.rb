41602c00c090b5765dd3b844f063b1619955b975
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-06-04 23:39:45 +0900

    06/04

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,10 @@
 5.times do |n|
-  puts n
-  User.create!(
-    name: "メンバー#{n + 1}",
-    email: "member#{n + 1}@test.com",
-    password: "test",
-    role: "member"
+  group = Group.create!(
+    name: "テストグループ#{n}",
+    description: "テストのグループです"
   )
-end
-
-User.create!(
-  name: 'メンター',
-  email: 'mentor@test.com',
-  password: 'test',
-  role: 'mentor'
-)
-
-Group.create!(
-  name: 'テストグループ',
-  description: 'テストのグループです'
-)
-
-5.times do |n|
-  Contribution.create!(
-    body: "テストの投稿#{n + 1}",
-    status: "new",
-    priority: "important",
-    user_id: n + 1,
-    group_id: 1
-  )
-end
-
-5.times do |n|
   UserGroup.create!(
-    user_id: n + 1,
-    group_id: 1
+    user_id: 7,
+    group_id: group.id
   )
 end
-
-
